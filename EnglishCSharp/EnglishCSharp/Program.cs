2f4114da0112deb07438a22e4e2dd98490e20602
Lucas Farias <lucasfarias@gmx.com>
2019-05-21 14:29:49 -0300

    Creates file for the first 4 classes

---
+++ EnglishCSharp/EnglishCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Console.WriteLine("Please type your first name.");
+        //string FirstName = Console.ReadLine();
+        //Console.WriteLine("Please type your last name.");
+        //string LastName = Console.ReadLine();
+        //Console.WriteLine ("Welcome {0}, {1}", FirstName, LastName);
+
+        Console.WriteLine("Max int = {0}", int.MaxValue);
+        Console.WriteLine("Min int = {0}", int.MinValue);
+    }
+}
