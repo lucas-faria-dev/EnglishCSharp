4f708ed5af6379402e64aa50f40facd2e3c8289e
Lucas de Freitas Lopes <lucasfarias@gmx.com>
2019-08-19 17:43:13 -0300

    19082019

---
+++ Part001/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main1()
+    {
+        Console.WriteLine("Hello world.1");
+        Console.WriteLine("Welcome to C# training!1");
+    }
+    static void Main()
+    {
+        Console.WriteLine("Hello world.");
+        Console.WriteLine("Welcome to C# training!");
+        Main1();
+    }
+
+
+}
