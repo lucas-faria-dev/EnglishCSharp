3dcacfb5ec6e3451e6b9eb8d0f513397764648dd
Lucas de Freitas Lopes <lucasfarias@gmx.com>
2019-08-21 17:19:56 -0300

    Starts class002

---
+++ Part002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+        }
+    }
+}
