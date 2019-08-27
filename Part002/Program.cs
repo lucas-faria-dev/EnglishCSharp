7b94c721e9918decf05eafc2d0ecd949a771e6f1
Lucas de Freitas Lopes <lucasfarias@gmx.com>
2019-08-27 13:30:43 -0300

    Finishes calss 002

--- Part002/Program.cs
+++ Part002/Program.cs
@@ -6,7 +6,14 @@ namespace Part002
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your name:");
+            Console.WriteLine("Please enter your first name:");
+            string vUserFirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name:");
+            string vUserLastName = Console.ReadLine();
+            
+            Console.WriteLine("Hello, {0} {1}", vUserFirstName,vUserLastName);
+            //Console.WriteLine("Welcome, "+vUser);
         }
     }
 }
